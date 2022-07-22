66d80229d4607db715d3bee922bcc2d63562a0d9
Johan Liebert <pyassistant00@gmail.com>
2022-07-22 03:14:01 +0000

    Update neovim config

--- lua/cmp_config.lua
+++ lua/cmp_config.lua
@@ -88,9 +88,6 @@ cmp.setup {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
   },
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-  },
   experimental = {
     ghost_text = false,
     native_menu = false,
