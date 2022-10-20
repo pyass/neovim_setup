da0e71ea643e3345433f8b16e805328109b59f5f
pyass <65035311+pyass@users.noreply.github.com>
2022-10-20 17:59:30 -0400

    fixed bug of missing wget

--- setup_neovim.sh
+++ setup_neovim.sh
@@ -1,6 +1,6 @@
-cd /opt && https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.tar.gz && tar xvzf nvim-linux64.tar.gz  && ln -s /opt/nvim-linux64/bin/nvim /usr/local/bin
+cd /opt && wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.tar.gz && tar xvzf nvim-linux64.tar.gz  && ln -s /opt/nvim-linux64/bin/nvim /usr/local/bin
 
-cd /opt && wget  https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz && tar xvzf ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz  && ln -s /opt/ripgrep-13.0.0-x86_64-unknown-linux-musl/rg /usr/local/bin
+cd /opt && wget https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz && tar xvzf ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz  && ln -s /opt/ripgrep-13.0.0-x86_64-unknown-linux-musl/rg /usr/local/bin
 
 git clone https://github.com/pyass/neovim_setup.git ~/.config/nvim
 
