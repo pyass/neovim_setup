72c787e04f837416008fb2c40c2515096b7e808d
pyass <pyassistant00@gmail.com>
2023-02-17 14:44:43 +0000

    downgrading from 0.83

--- lua/lsp_install.lua
+++ lua/lsp_install.lua
@@ -10,21 +10,9 @@ end
 
 mason.setup()
 
-mason_config.setup()
+mason_config.setup {
+    ensure_installed = { "pyright", "tsserver", "lua_ls", "bash_ls" },
+    automatic_installion = true
+}
 
-lsp_installer.on_server_ready(function(server)
-	local opts = {
-		on_attach = require("handlers").on_attach,
-		capabilities = require("handlers").capabilities,
-	}
-
-	 if server.name == "pyright" then
-	 	local pyright_opts = require("settings.pyright")
-	 	opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-	 end
-
-	-- This setup() function is exactly the same as lspconfig's setup function.
-	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-	server:setup(opts)
-end)
 
--- setup_neovim.sh
+++ setup_neovim.sh
@@ -1,4 +1,4 @@
-cd /opt && sudo wget https://github.com/neovim/neovim/releases/download/v0.8.3/nvim-linux64.tar.gz && sudo tar xvzf nvim-linux64.tar.gz  && sudo ln -s /opt/nvim-linux64/bin/nvim /usr/local/bin
+cd /opt && sudo wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.tar.gz && sudo tar xvzf nvim-linux64.tar.gz  && sudo ln -s /opt/nvim-linux64/bin/nvim /usr/local/bin
 
 cd /opt && sudo wget https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz && sudo tar xvzf ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz  && sudo ln -s /opt/ripgrep-13.0.0-x86_64-unknown-linux-musl/rg /usr/local/bin
 
