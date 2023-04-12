d2955f5f8e3f4a1e19d437b42eb75cdabf11b227
pyass <pyassistant00@gmail.com>
2023-04-11 21:08:25 -0400

    update quickfix movement

--- lua/shortcuts.lua
+++ lua/shortcuts.lua
@@ -28,7 +28,7 @@ vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-i>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>nn", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>np", "<cmd>lprev<CR>zz")
