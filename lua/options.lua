21e1599e771526d0c0789c43c99287254417adaf
pyass <pyassistant00@gmail.com>
2023-05-18 20:18:54 +0000

    increment letters

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,13 @@
 vim.g.mapleader = ","
-
+vim.o.tabstop=4
+vim.o.softtabstop=4
+vim.o.errorbells=false
+vim.o.shiftwidth=4
+vim.o.expandtab=true
+vim.o.smartindent=true
+vim.o.number=true
+vim.o.relativenumber=true
+vim.o.wrap=false
 vim.o.tabstop=4
 vim.o.softtabstop=4
 vim.o.errorbells=false
@@ -18,6 +26,7 @@ vim.o.undodir= vim.fn.stdpath('config') .. '/undo'
 vim.o.undofile=true
 vim.o.compatible=false
 vim.o.backspace='indent,eol,start'
+vim.o.nrformats='hex,alpha'
 vim.o.termguicolors=true
 local theme, _ = pcall(require, "tokyonight")
 if not theme then
