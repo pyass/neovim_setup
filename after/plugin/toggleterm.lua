0245f337afe273dc37c14915bf887fad9edc7c8d
pyass <pyassistant00@gmail.com>
2023-08-12 19:45:44 +0000

    updated toggleterm

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -36,7 +36,7 @@ function _G.set_terminal_keymaps()
     vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
     vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
     vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-    vim.api.nvim_buf_set_keymap(0, 't', '<C-s>', "<cmd>ToggleTerm<cr>", opts)
+    vim.api.nvim_buf_set_keymap(0, 't', '<C-x>', "<cmd>ToggleTerm<cr>", opts)
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
