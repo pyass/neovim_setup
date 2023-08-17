5e3e73039859ec4673fde789504178c88f24ee16
pyass <pyassistant00@gmail.com>
2023-08-17 20:12:12 +0000

    added close location list (finally)

--- lua/shortcuts.lua
+++ lua/shortcuts.lua
@@ -40,4 +40,6 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>nn", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>np", "<cmd>lprev<CR>zz")
 
+vim.keymap.set("n", "<leader>cl", "<cmd>:lclose<CR>")
+
 vim.keymap.set("n", "<leader>re", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
