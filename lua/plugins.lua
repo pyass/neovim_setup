56a588ee029784e51d38f2f20eabf3c1a66a1020
pyass <pyassistant00@gmail.com>
2024-03-08 21:51:20 -0500

    updated formatter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -71,6 +71,17 @@ require("lazy").setup({
 
 	{ -- Autoformat
 		"stevearc/conform.nvim",
+		keys = {
+			{
+				-- Customize or remove this keymap to your liking
+				"<leader>fo",
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				mode = "",
+				desc = "Format buffer",
+			},
+		},
 		opts = {
 			notify_on_error = false,
 			format_on_save = {
@@ -79,7 +90,6 @@ require("lazy").setup({
 			},
 			formatters_by_ft = {
 				lua = { "stylua" },
-				-- Conform can also run multiple formatters sequentially
 				python = { "black" },
 				--
 				-- You can use a sub-list to tell conform to run *until* a formatter
