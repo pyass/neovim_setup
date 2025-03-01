33ed83f09ac3bb8564262279e140d4d970f7ee2d
pyass <pyassistant00@gmail.com>
2025-03-01 04:45:45 +0000

    updated nvim location

--- setup_neovim.sh
+++ setup_neovim.sh
@@ -1,13 +1,7 @@
-cd /opt && sudo wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage && sudo chmod +x /opt/nvim.appimage && sudo ./nvim.appimage --appimage-extract && sudo ln -s /opt/squashfs-root/usr/bin/nvim /usr/local/bin
+cd /opt && sudo wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.appimage && sudo mv nvim-linux-x86_64.appimage nvim.appimage && sudo chmod +x /opt/nvim.appimage && sudo ./nvim.appimage --appimage-extract && sudo ln -s /opt/squashfs-root/usr/bin/nvim /usr/local/bin
 
 cd /opt && sudo wget https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz && sudo tar xvzf ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz  && sudo ln -s /opt/ripgrep-13.0.0-x86_64-unknown-linux-musl/rg /usr/local/bin
 
 git clone https://github.com/pyass/neovim_setup.git ~/.config/nvim
 
 cd /tmp && git clone https://github.com/pyass/dev-tools.git && bash /tmp/dev-tools/setup.sh
-
-pip3 install debugpy
-
-curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
-
-echo 'eval "$(zoxide init bash)"' >> ~/.bashrc
