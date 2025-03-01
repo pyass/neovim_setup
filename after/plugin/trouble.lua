c6d422ac1cef0fb653d8ef5c6ee551b38d045a87
pyass <pyassistant00@gmail.com>
2025-03-01 04:40:11 +0000

    updated trouble

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 trouble.setup {
-    icons = false,
+    icons = {},
     fold_open = "v",      -- icon used for open folds
     fold_closed = ">",    -- icon used for closed folds
     indent_lines = false, -- add an indent guide below the fold icons
@@ -21,12 +21,9 @@ trouble.setup {
     use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
 }
 
-vim.keymap.set("n", "<C-d>", function() require("trouble").toggle("diagnostics") end)
-vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
-vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
-vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
-vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
-vim.keymap.set("n", "<leader>xr", function() require("trouble").toggle("lsp_references") end)
-
-vim.keymap.set("n", "<leader>xn", function() require("trouble").next({ skip_groups = true, jump = true }) end)
-vim.keymap.set("n", "<leader>xp", function() require("trouble").previous({ skip_groups = true, jump = true }) end)
+vim.keymap.set("n", "<C-d>", "<cmd>Trouble diagnostics toggle<cr>")
+vim.keymap.set("n", "<leader>xd", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>")
+vim.keymap.set("n", "<leader>xq", "<cmd>Trouble qflist toggle<cr>")
+vim.keymap.set("n", "<leader>xl", "<cmd>Trouble loclist toggle<cr>")
+vim.keymap.set("n", "<leader>xr", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>")
+vim.keymap.set("n", "<leader>xs", "<cmd>Trouble symbols toggle<cr>")
